19b712538fe686bb7136e59634e2b31b58b8a0d6
imskanand <the.anand3099@gmail.com>
2022-04-29 23:02:15 +0530

    function parameter

--- bin/function.dart
+++ bin/function.dart
@@ -10,3 +10,22 @@ void main(List<String> args) {
 String welcomeAll() {
   return 'Welcome All';
 }
+
+void positionalParameter(int x, double y, String greetings) {
+  positionalParameter(5, 5.5, 'hi');
+}
+
+void optionalPositionalParameter(int x, double y, [String? greetings]) {
+  optionalPositionalParameter(5, 4.3);
+  optionalPositionalParameter(3, 6.6, 'hello');
+}
+
+void namedOptionalParameter({int? x, double? y, String? greetings}) {
+  namedOptionalParameter(x: 5, y: 5.6, greetings: 'hi, there!!');
+}
+
+void namedRequiredOptionalParameter(int positional,
+    {required int x, required double y, required String greetings}) {
+  namedRequiredOptionalParameter(101,
+      x: 4, y: 4.6, greetings: 'Oh! Hi there..');
+}
